22bbaab0d6d03f58912f028896967e7e7b731b55
Delfio <delfio_eu@hotmail.com>
2019-09-03 20:17:46 -0400

    chore: Altera Res

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res)=>{
-  return res.send('Hello World!')
+app.get("/", (req, res) => {
+  return res.send("Hello World2!");
 });
 
 app.listen(3333);
